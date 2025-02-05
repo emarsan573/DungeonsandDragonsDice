7f8dee2dbf26a5dcade7c541b1e58fa24e305c44
Eric Marsan <ericmarsan573@gmail.com>
2025-02-05 14:19:17 -0500

    Edited code so script only rolls 1 die instead of 2 dice

--- DnDDiceMasterFile.py
+++ DnDDiceMasterFile.py
@@ -39,13 +39,13 @@ dice_drawing = {
 def roll_dice():
     roll = input("Roll the dice? (y/n) : ")
 
-    while roll.lower() == "y".lower():
+    #while roll.lower() == "y".lower():
         dice1 = random.randint(1, 4)
-        dice2 = random.randint(1, 4)
+        #dice2 = random.randint(1, 4)
 
-        print("dice rolled {} and {}".format(dice1, dice2))
+        print("die rolled {}".format(dice1))
         print("\n".join(dice_drawing[dice1]))
-        print("\n".join(dice_drawing[dice2]))
+        #print("\n".join(dice_drawing[dice2]))
 
         roll = input("\nRoll again? (y/n): ")
 
